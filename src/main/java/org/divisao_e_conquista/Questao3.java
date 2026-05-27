189c6be01033f04c480dc92c631c61b682678a9e
Kreucyu <elciocleiton72@gmail.com>
2026-05-27 12:04:52 -0300

    test: teste final da questão

--- src/main/java/org/divisao_e_conquista/Questao3.java
+++ src/main/java/org/divisao_e_conquista/Questao3.java
@@ -4,17 +4,23 @@ import org.divisao_e_conquista.algorithm.Algoritmo;
 
 public class Questao3 {
     public static void main(String[] args) {
-        System.out.println("\nQuestão 3:");
+        System.out.println("\nQuestão 3:\n");
         String texto = "FURB";
         String[] vetorString = texto.split("");
         String texto1 = "PARALELEPIPEDO";
         String[] vetorString1 = texto1.split("");
         String texto2 = "HELLO WORLD";
         String[] vetorString2 = texto2.split("");
+        String palavrasReversas = "BRUF\n" +
+                "ODEPIPELELARAP\n" +
+                "DLROW OLLEH";
+        String[] vetorString3 = palavrasReversas.split("");
+
 
         Algoritmo divisaoEConquista4 = new Algoritmo(null);
         System.out.println(divisaoEConquista4.inverterString(vetorString));
         System.out.println(divisaoEConquista4.inverterString(vetorString1));
         System.out.println(divisaoEConquista4.inverterString(vetorString2));
+        System.out.println("\n" + divisaoEConquista4.inverterString(vetorString3));
     }
 }
