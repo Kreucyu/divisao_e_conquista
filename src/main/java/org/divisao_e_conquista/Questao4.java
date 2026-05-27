cfafdd826f74dadd9b86455cc5fb8e7ffea2633d
Kreucyu <elciocleiton72@gmail.com>
2026-05-27 13:01:35 -0300

    feat: implementando Main

--- src/main/java/org/divisao_e_conquista/Questao4.java
+++ src/main/java/org/divisao_e_conquista/Questao4.java
@@ -1,4 +1,19 @@
 package org.divisao_e_conquista;
 
+import org.divisao_e_conquista.algorithm.Algoritmo;
+
 public class Questao4 {
+    public static void main(String[] args) {
+        System.out.println("\nQuestão 4:\n");
+        int[] vetor1 = {1, 2, 1, 1, 3};
+        int[] vetor2 = {1, 2, 0, 2, 2, 3, 2, 7, 2, 1, 2, 1, 3, 2, 2};
+        int[] vetor3 = {1, 1, 2, 5, 2, 0};
+
+        Algoritmo algoritmo = new Algoritmo(vetor1);
+        Algoritmo algoritmo1 = new Algoritmo(vetor2);
+        Algoritmo algoritmo2 = new Algoritmo(vetor3);
+
+        System.out.println(algoritmo.encontrarMaioria());
+
+    }
 }
