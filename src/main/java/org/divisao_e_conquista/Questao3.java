5caeea8ca30e47163053b22806c88c5fc7f11e95
Kreucyu <elciocleiton72@gmail.com>
2026-05-27 12:02:56 -0300

    test: testando palavras invertidas

--- src/main/java/org/divisao_e_conquista/Questao3.java
+++ src/main/java/org/divisao_e_conquista/Questao3.java
@@ -7,8 +7,14 @@ public class Questao3 {
         System.out.println("\nQuestão 3:");
         String texto = "FURB";
         String[] vetorString = texto.split("");
+        String texto1 = "PARALELEPIPEDO";
+        String[] vetorString1 = texto1.split("");
+        String texto2 = "HELLO WORLD";
+        String[] vetorString2 = texto2.split("");
 
         Algoritmo divisaoEConquista4 = new Algoritmo(null);
         System.out.println(divisaoEConquista4.inverterString(vetorString));
+        System.out.println(divisaoEConquista4.inverterString(vetorString1));
+        System.out.println(divisaoEConquista4.inverterString(vetorString2));
     }
 }
